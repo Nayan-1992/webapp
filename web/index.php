598c514c71d8fd1abd223225e66d02692669b0a4
Nayan-1992 <48062037+Nayan-1992@users.noreply.github.com>
2020-08-02 14:40:39 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "<h1 align='center'> Hello Theere Welcome to ECS Test </h1>";
-  echo "<h3 align='center'> PLease find the php info below </h3>";
+  echo "<h1 align='center'> Hello There Welcome to ECS Test </h1>";
+  echo "<h3 align='center'> Pease find the php info below </h3>";
   phpinfo();
 ?>
