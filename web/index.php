5a4fa14488a528887ed2987c287e5df07c5f31c9
Nayan-1992 <48062037+Nayan-1992@users.noreply.github.com>
2020-08-02 15:11:14 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "<h1> Version :18 </h1>";
+  echo "<h1> Version :19 </h1>";
   echo "<h1 align='center'> Hello There Welcome to ECS Test </h1>";
   echo "<h3 align='center'> Pease find the php info below </h3>";
   phpinfo();
